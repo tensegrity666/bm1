c822d4951e59ea3aeef3e2d48f80a686edcda9d2
Roman Yakovlev <tensegrity666@eicloud.com>
2019-11-25 20:33:39 +0300

    Deletes all SASS files, folders and dependencies, as SASS is not used

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,12 @@
 "use strict";
 
 const { src, dest, watch } = require("gulp");
-const sass = require("gulp-sass");
-const csso = require("gulp-csso");
-const rename = require("gulp-rename");
-const plumber = require("gulp-plumber");
-const beeper = require("beeper");
 const stylelint = require("gulp-stylelint");
 const browserSync = require("browser-sync").create();
 const reload = browserSync.reload;
 
-function style() {
-  return src("source/sass/style.scss")
-    .pipe(plumber(errorHandler))
-    .pipe(sass({
-      includePaths: require('node-normalize-scss').includePaths
-    }))
-    .pipe(csso())
-    .pipe(rename("style.min.css"))
-    .pipe(dest("source/css"))
-    .pipe(browserSync.stream());
-};
-
-function errorHandler(error) {
-  beeper();
-  return true;
-}
-
 function lint() {
-  return src("source/sass/**/*.scss")
+  return src("source/**/*.css")
     .pipe(stylelint({
       reporters: [{ formatter: 'string', console: true }]
     }))
@@ -42,10 +20,9 @@ function server() {
     cors: true,
     ui: false
   });
-  watch("source/sass/**/*.scss", style);
+  watch("source/**/*.css").on("change", reload);
   watch("source/*.html").on("change", reload);
 };
 
-exports.style = style;
 exports.lint = lint;
 exports.server = server;
