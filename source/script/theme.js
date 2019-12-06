b710133c197161e5a169381fbdb55f92ac759568
Roman Yakovlev <tensegrity666@eicloud.com>
2019-12-07 00:36:41 +0300

    First implementation of theme toggler

--- source/script/theme.js
+++ source/script/theme.js
@@ -1,6 +1,6 @@
 'Use strict';
 
-  const theme = document.querySelector('.onoffswitch_cheked');
+  const  = document.querySelector('_cheked');
   const themeInvert = document.querySelector('.theme');
 
 theme.addEventListener('click', function (evt) {
@@ -8,3 +8,14 @@ theme.addEventListener('click', function (evt) {
   themeInvert.classList.add('theme_color_project-invert');
   theme.classList.remove('onoffswitch_cheked');
   });
+
+
+{
+  const themeToggle = document.querySelector(".onoffswitch");
+  const themeInvert = document.querySelector(".theme");
+
+  themeToggle.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    themeInvert.classList.add("theme_color_project-invert");
+  });
+}
