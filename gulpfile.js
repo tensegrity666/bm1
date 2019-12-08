26a13b1e49fa19d4ad80e81f448fe4e25c56bc3a
Roman Yakovlev <tensegrity666@eicloud.com>
2019-12-08 16:08:22 +0300

    Adds js concat task

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,16 @@ const stylelint = require("gulp-stylelint");
 const browserSync = require("browser-sync").create();
 const gulpConcat = require("gulp-concat");
 
-function concat() {
+function style() {
   return src("source/common.blocks/**/*.css")
     .pipe(gulpConcat("main.css"))
+    .pipe(dest("build/"))
+    .pipe(browserSync.stream());
+};
+
+function script() {
+  return src("source/scripts/*.js")
+    .pipe(gulpConcat("main.js"))
     .pipe(dest("build/"))
     .pipe(browserSync.stream());
 };
@@ -20,9 +27,10 @@ function server() {
     cors: true,
     ui: false
   });
-  watch("source/common.blocks/**/*.css", concat);
+  watch("source/common.blocks/**/*.css", style);
   watch("*.html").on("change", browserSync.reload);
 };
 
-exports.concat = concat;
+exports.style = style;
+exports.script = script;
 exports.server = server;
