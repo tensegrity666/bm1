9c1941e4a75c777fdbe538fd0617ee92652a9c02
Roman Yakovlev <tensegrity666@eicloud.com>
2019-12-12 01:11:11 +0300

    version with body.addEventListener

--- build/script.js
+++ build/script.js
@@ -1,37 +1,30 @@
 "Use strict";
 
-const bodyOnload = document.body.onload = () => {
-  const themeToggle = document.querySelector(".onoffswitch");
+document.body.addEventListener("click", function (event) {
+  if (event.target.classList.contains("onoffswitch")) {
+    event.target.classList.toggle("onoffswitch_checked");
 
-  themeToggle.onclick = () => {
     const theme = document.querySelectorAll(".theme");
-    const body = document.querySelector("body");
-
-    themeToggle.classList.toggle("onoffswitch_checked");
 
     for (let i = 0; i < theme.length; ++i) {
       if (theme[i].classList.contains("theme_color_project-inverse")) {
         theme[i].classList.toggle("theme_color_project-inverse")
         theme[i].classList.add("theme_color_project-default")
-      } else if (theme[i].classList.contains("theme_color_project-brand")) {
+      }
+      else if (theme[i].classList.contains("theme_color_project-brand")) {
         theme[i].classList.remove("theme_color_project-inverse")
         theme[i].classList.remove("theme_color_project-default")
-      } else
+      }
+      else
         theme[i].classList.toggle("theme_color_project-inverse");
     };
 
-    if (body.classList.contains("theme", "theme_color_project-inverse"))
-      body.classList.remove("theme", "theme_color_project-inverse")
+    if (this.classList.contains("theme", "theme_color_project-inverse"))
+      this.classList.remove("theme", "theme_color_project-inverse")
     else
-      body.classList.add("theme", "theme_color_project-inverse");
+      this.classList.add("theme", "theme_color_project-inverse");
   };
 
-  const accordionToggle = document.querySelectorAll(".history__transaction");
-  const accordionHide = document.querySelectorAll(".history__hide");
-
-  for (let i = 0; i < accordionToggle.length; ++i) {
-    accordionToggle[i].onclick = () => {
-    accordionHide[i].classList.toggle("history__hide");
-    };
-  };
-};
+  if (event.target.classList.contains("history"))
+    event.target.classList.toggle("history__hide");
+});
