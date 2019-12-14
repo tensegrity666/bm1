597370a0ef184927f2bec66bb0ba33336d6e775e
Roman Yakovlev <tensegrity666@eicloud.com>
2019-12-14 18:23:26 +0300

    Trial with window.onLoad

--- build/script.js
+++ build/script.js
@@ -1,30 +1,33 @@
 "Use strict";
 
-document.addEventListener("click", function (event) {
-  if (event.target.classList.contains("onoffswitch")) {
-    event.target.classList.toggle("onoffswitch_checked");
+window.onload = function() {
 
-    const theme = document.querySelectorAll(".theme");
+  document.body.addEventListener("click", function (event) {
+    if (event.target.classList.contains("onoffswitch")) {
+      event.target.classList.toggle("onoffswitch_checked");
 
-    for (let i = 0; i < theme.length; ++i) {
-      if (theme[i].classList.contains("theme_color_project-inverse")) {
-        theme[i].classList.toggle("theme_color_project-inverse")
-        theme[i].classList.add("theme_color_project-default")
-      }
-      else if (theme[i].classList.contains("theme_color_project-brand")) {
-        theme[i].classList.remove("theme_color_project-inverse")
-        theme[i].classList.remove("theme_color_project-default")
-      }
-      else
-        theme[i].classList.toggle("theme_color_project-inverse");
+      const theme = document.querySelectorAll(".theme");
+
+      for (let i = 0; i < theme.length; ++i) {
+        if (theme[i].classList.contains("theme_color_project-inverse")) {
+          theme[i].classList.toggle("theme_color_project-inverse")
+          theme[i].classList.add("theme_color_project-default")
+        } else if (theme[i].classList.contains("theme_color_project-brand")) {
+          theme[i].classList.remove("theme_color_project-inverse")
+          theme[i].classList.remove("theme_color_project-default")
+        } else {
+          theme[i].classList.toggle("theme_color_project-inverse");
+        }
+      };
+
+      if (!this.classList.contains("theme")) {
+        this.classList.add("theme");
+        this.classList.add("theme_color_project-inverse");
+      };
     };
 
-    if (this.classList.contains("theme", "theme_color_project-inverse"))
-      this.classList.remove("theme", "theme_color_project-inverse")
-    else
-      this.classList.add("theme", "theme_color_project-inverse");
-  };
+    if (event.target.classList.contains("history"))
+      event.target.classList.toggle("history__hide");
+  });
+}
 
-  if (event.target.classList.contains("history"))
-    event.target.classList.toggle("history__hide");
-});
