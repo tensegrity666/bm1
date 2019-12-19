cfb1554745bc8a3ce1cbc5152fabf1ce77745c00
Roman Yakovlev <tensegrity666@eicloud.com>
2019-12-19 20:37:41 +0300

    Fixes accordion

--- build/script.js
+++ build/script.js
@@ -26,8 +26,13 @@ window.onload = function() {
       };
     };
 
-    if (event.target.classList.contains("history"))
-      event.target.classList.toggle("history__hide");
+
+    let accordionEvent = event.target.closest(".history__transaction");
+    if (accordionEvent) {
+      let accordionHide = accordionEvent.children;
+        accordionHide[1].classList.toggle("history__hide"); //modificator history__hide always has index 1
+    };
+
   });
-}
+};
 
