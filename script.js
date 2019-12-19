78d8a6acd748c6e98976f60c3e2355268e6f06a4
Roman Yakovlev <tensegrity666@eicloud.com>
2019-12-20 00:16:13 +0300

    Adds script.js to src

---
+++ script.js
@@ -0,0 +1,34 @@
+"Use strict";
+
+window.onload = function() {
+
+  document.body.addEventListener("click", function (event) {
+    if (event.target.classList.contains("onoffswitch")) {
+      event.target.classList.toggle("onoffswitch_checked");
+
+      const theme = document.querySelectorAll(".theme");
+      for (let i = 0; i < theme.length; ++i) {
+        if (theme[i].classList.contains("theme_color_project-inverse")) {
+          theme[i].classList.toggle("theme_color_project-inverse")
+          theme[i].classList.add("theme_color_project-default")
+        } else if (theme[i].classList.contains("theme_color_project-brand")) {
+          theme[i].classList.remove("theme_color_project-inverse")
+          theme[i].classList.remove("theme_color_project-default")
+        } else {
+          theme[i].classList.toggle("theme_color_project-inverse");
+        }
+      };
+
+      if (!this.classList.contains("theme")) {
+        this.classList.add("theme");
+        this.classList.add("theme_color_project-inverse");
+      };
+    };
+
+    let accordionEvent = event.target.closest(".history__transaction");
+    if (accordionEvent) {
+    let accordionHide = accordionEvent.children;
+    accordionHide[1].classList.toggle("history__hide"); //modificator history__hide always has index 1
+    };
+  });
+};
